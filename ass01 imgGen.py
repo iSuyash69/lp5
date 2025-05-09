4e54ccb3540ddf4867dacac500e8ce43916420ca
Suyash Deshpande <97775667+iSuyash69@users.noreply.github.com>
2025-05-09 18:11:43 +0530

    Update ass01 imgGen.py

--- ass01 imgGen.py
+++ ass01 imgGen.py
@@ -3,7 +3,7 @@ import io
 from PIL import Image
 
 
-# API_TOKEN="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"
+API_TOKEN="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"
 
 API_URL="https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0"
 
